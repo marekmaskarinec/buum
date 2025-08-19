629b382197ab806338e39289e644cd47492b0f90
Marek Maškarinec <marek@mrms.cz>
2025-08-19 22:25:56 +0200

    Add build zig run
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,10 @@ fn handleError(header: []const u8, inst: umka.Instance) void {
 }
 
 fn printHelpAndExit(status: u8) void {
-    std.io.getStdOut().writeAll("buum - an Umka builder\n" ++ "\t-C <dir>\t-- change root directory\n" ++ "\t-h\t-- show this help message\n") catch {};
+    std.io.getStdOut().writeAll("buum - an Umka builder\n" ++
+        "\t-C <dir>\t-- change root directory\n" ++
+        "\t-g\t-- only generate\n" ++
+        "\t-h\t-- show this help message\n") catch {};
     std.process.exit(status);
 }
 
@@ -35,32 +38,26 @@ fn runBuildUm() ![]const u8 {
 
     try instance.compile();
 
-    // var build: Build = .{
-    //     .outPath = "buum/build.zig",
-    //     .data = undefined,
-    // };
-    var build_mem = try instance.allocData(@sizeOf(Build), onFree);
-    var build: *Build = @ptrCast(@alignCast(build_mem.ptr));
-    build.outPath = "buum/build.zig";
+    var build: Build = .{
+        .outPath = "build.zig",
+        .data = undefined,
+    };
 
     var initFunc = try instance.getFunc("bu.um", "__init");
     var func = try instance.getFunc("build.um", "build");
     var deinitFunc = try instance.getFunc("bu.um", "__deinit");
 
-    initFunc.setParameters(&.{.{ .ptr = build }});
-    build_mem.incRef();
+    initFunc.setParameters(&.{.{ .ptr = &build }});
     try initFunc.call();
     var ec = initFunc.getResult().int;
     if (ec != 0) {
         std.log.err("Initializing build failed with code {d}", .{ec});
     }
 
-    func.setParameters(&.{.{ .ptr = build }});
-    build_mem.incRef();
+    func.setParameters(&.{.{ .ptr = &build }});
     try func.call();
 
-    deinitFunc.setParameters(&.{.{ .ptr = build }});
-    build_mem.incRef();
+    deinitFunc.setParameters(&.{.{ .ptr = &build }});
     try deinitFunc.call();
     ec = deinitFunc.getResult().int;
     if (ec != 0) {
@@ -70,7 +67,23 @@ fn runBuildUm() ![]const u8 {
     return std.mem.sliceTo(build.outPath, 0);
 }
 
+fn runBuildZig(gpa: std.mem.Allocator, path: []const u8) !u8 {
+    // TODO: Make this basepath(path)/out
+    try std.fs.cwd().makePath("buum/out");
+
+    // TODO: Embedded Zig
+    const args = [_][]const u8{ "zig", "build", "--build-file", path, "--prefix", "buum/out" };
+    var cmd = std.process.Child.init(&args, gpa);
+    const term = try std.process.Child.spawnAndWait(&cmd);
+    return switch (term) {
+        .Exited => |ec| ec,
+        else => error{InvalidTerm}.InvalidTerm,
+    };
+}
+
 pub fn main() !void {
+    var genOnly = false;
+
     var args = std.process.args();
     _ = args.next();
     while (args.next()) |arg| {
@@ -84,6 +97,8 @@ pub fn main() !void {
                 std.log.err("usage: buum -C <directory>", .{});
                 std.process.exit(1);
             }
+        } else if (std.mem.eql(u8, arg, "-g")) {
+            genOnly = true;
         } else if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
             printHelpAndExit(0);
         } else {
@@ -92,7 +107,15 @@ pub fn main() !void {
         }
     }
 
-    try std.fs.cwd().makePath("buum");
     const buildZigPath = try runBuildUm();
-    _ = buildZigPath;
+
+    if (genOnly) {
+        std.log.info("Result saved to {s}", .{buildZigPath});
+        return;
+    }
+
+    var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = gpa_impl.allocator();
+
+    std.process.exit(try runBuildZig(gpa, buildZigPath));
 }

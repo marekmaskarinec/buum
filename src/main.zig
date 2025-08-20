3ab90a32c4a56723385b8f9806400266b1385544
Marek Maškarinec <marek@mrms.cz>
2025-08-20 21:31:26 +0200

    Add target support
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,42 @@ const umka = @import("umka");
 
 const bu_um = @embedFile("bu.um");
 
+const Target = enum(c_int) {
+    default = 0,
+    linux = 1,
+    linux_musl = 2,
+    linux_glibc = 3,
+    windows = 4,
+    emscripten = 5,
+};
+
+fn UmkaDynArray(comptime T: type) type {
+    return extern struct {
+        internal: *anyopaque,
+        itemSize: i64,
+        data: [*]T,
+
+        extern fn umkaGetDynArrayLen(arr: *anyopaque) c_int;
+
+        pub fn len(self: *@This()) usize {
+            return @intCast(umkaGetDynArrayLen(@ptrCast(self)));
+        }
+
+        pub fn slice(self: *@This()) []T {
+            return self.data[0..self.len()];
+        }
+    };
+}
+
 fn handleError(header: []const u8, inst: umka.Instance) void {
     const err = inst.getError();
     std.debug.print("{s} Error: {s}:{s}:{d}:{d}: {s}\n", .{ header, err.file_name, err.fn_name, err.line, err.pos, err.msg });
 }
 
 fn printHelpAndExit(status: u8) void {
-    std.io.getStdOut().writeAll("buum - an Umka builder\n" ++
+    std.io.getStdErr().writeAll("buum - an Umka builder\n" ++
         "\t-C <dir>\t-- change root directory\n" ++
+        "\t-t <targets>\t-- set build targets\n" ++
         "\t-g\t-- only generate\n" ++
         "\t-h\t-- show this help message\n") catch {};
     std.process.exit(status);
@@ -20,6 +48,18 @@ fn onFree(p: [*]umka.StackSlot, r: *umka.StackSlot) callconv(.C) void {
     _ = p;
     _ = r;
     std.log.warn("Free", .{});
+}
+
+var targets: std.ArrayList(Target) = undefined;
+
+extern fn umkaMakeDynArray(self: *anyopaque, arr: *anyopaque, type: *umka.Type, size: c_int) callconv(.c) void;
+
+fn umc__getTargets(params: [*]umka.StackSlot, result: *umka.StackSlot) callconv(.c) void {
+    const typeptr: *umka.Type = params[1].ptr;
+    const arr: *UmkaDynArray(Target) = @ptrCast(@alignCast(params[0].ptr));
+
+    umkaMakeDynArray(result.ptr, @ptrCast(arr), typeptr, @intCast(targets.items.len));
+    std.mem.copyForwards(Target, arr.slice(), targets.items);
 }
 
 fn runBuildUm() ![]const u8 {
@@ -35,6 +75,8 @@ fn runBuildUm() ![]const u8 {
     std.debug.assert(instance.alive());
 
     try instance.addModule("bu.um", bu_um);
+
+    try instance.addFunc("umc__getTargets", &umc__getTargets);
 
     try instance.compile();
 
@@ -82,7 +124,12 @@ fn runBuildZig(gpa: std.mem.Allocator, path: []const u8) !u8 {
 }
 
 pub fn main() !void {
+    var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = gpa_impl.allocator();
+
     var genOnly = false;
+    targets = std.ArrayList(Target).init(gpa);
+    defer targets.deinit();
 
     var args = std.process.args();
     _ = args.next();
@@ -101,6 +148,32 @@ pub fn main() !void {
             genOnly = true;
         } else if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
             printHelpAndExit(0);
+        } else if (std.mem.eql(u8, arg, "-t")) {
+            if (args.next()) |next| {
+                var iter = std.mem.splitScalar(u8, next, ',');
+                while (iter.next()) |s| {
+                    var target: ?Target = null;
+                    inline for (std.meta.fields(Target)) |f| {
+                        if (std.mem.eql(u8, s, f.name)) {
+                            target = @enumFromInt(f.value);
+                            break;
+                        }
+                    }
+
+                    if (target) |t| {
+                        try targets.append(t);
+                    } else {
+                        std.log.err("unknown target {s}, available targets: ", .{s});
+                        inline for (std.meta.fields(Target)) |f| {
+                            std.log.err("\t{s}", .{f.name});
+                        }
+                        std.process.exit(1);
+                    }
+                }
+            } else {
+                std.log.err("usage: buum -t <target list>", .{});
+                std.process.exit(1);
+            }
         } else {
             std.log.err("invalid argument {s}", .{arg});
             printHelpAndExit(1);
@@ -113,9 +186,6 @@ pub fn main() !void {
         std.log.info("Result saved to {s}", .{buildZigPath});
         return;
     }
-
-    var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = gpa_impl.allocator();
 
     std.process.exit(try runBuildZig(gpa, buildZigPath));
 }

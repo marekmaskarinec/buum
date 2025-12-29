80c876a89bde1af8e7ce552aa1e9730ade4f2d0e
Marek Maškarinec <marek@mrms.cz>
2025-12-29 23:36:04 +0100

    Add fs tests to CI
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- test/fs/run.sh
+++ test/fs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source ./utils.sh
+. ./utils.sh
 
 PASS=0
 TOTAL=0
@@ -10,15 +10,21 @@ for d in */; do
   echo "$d" | grep -q '^f_'
   should_fail=$?
   cd $d
-  [ -n "$CLEAR_CACHE" ] && rm -r buum/cache
-  rm -r buum/out
-  ../../../zig-out/bin/buum
+  [ -n "$CLEAR_CACHE" ] && rm -rf buum/cache
+  rm -rf buum/out
+  if [ $should_fail ]; then
+    ../../../zig-out/bin/buum 2>/dev/null
+  else
+    ../../../zig-out/bin/buum
+  fi
   if [ "$?" -ne "$should_fail" ] && sh verify.sh; then
     PASS=$(( PASS + 1 ))
     echo "PASS $d"
+    cd ..
     continue
   fi
   echo "FAIL $d"
+  cd ..
 done
 
 echo $PASS out of $TOTAL passed
